0da7003894dbdb3e16405ba10fbc09e4b83bc812
Dan Senga <dansenga455@gmail.com>
2024-09-06 21:09:52 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector('#menu-icone')
-const navLinks = document.querySelector('.nav-Links')
-
-menuIcon.onclick = () => {
-    navLinks.classList.toggle('active');
-}
